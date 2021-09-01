3b868ac3cf9d0a1d3ada01eaa9f9bddc43cc7ee6
scotchwsplenda <lockwood.oliver@gmail.com>
2021-09-01 14:29:06 -0700

    fucking damnit

--- yachfitters_site/yachfitters_site/settings.py
+++ yachfitters_site/yachfitters_site/settings.py
@@ -122,5 +122,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 
 LOCKDOWN_PASSWORDS = ('$bXLVIII43-8')
